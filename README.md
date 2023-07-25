c56641b769a57175302c5f503fd55a318f5fb431
Arcueid <tinfireii2002@gmail.com>
2023-07-25 00:17:11 -0400

    Updated the read me and added to the main

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # isv-calculator
 PRSK calculator
+bing bing bing bon bon!
+
+Simple python code thatwill calculate the best room order using ISV
+
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 
+names_unparsed = input("Enter the names separated with space : ") 
+
+isv_unparsed = input("Enter isv separated with space: ")
+
+isv = isv_unparsed.split()
+names = names_unparsed.split()
+
+for n in names:
+    print(n)
 
 
+for x in isv:
+    print(x)
