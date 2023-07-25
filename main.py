8da710a60e3fb8f7addf4931c53393dc0a49e96a
Arcueid <tinfireii2002@gmail.com>
2023-07-25 14:46:12 -0400

    ERROR HANDLING!??!

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-
-#names_unparsed = input("Enter the names separated with space : ") 
-
-#$isv_unparsed = input("Enter isv separated with space: ")
-
 isv = []
 names = []
-
 def main():
+    
+
     running = True
     counter = 5
     while running:
-        user_input = input("Enter name followed by ISV or Q to quit ")
+        user_input = input("Enter name followed by ISV or Q to quit ") 
         if user_input.upper() == "Q":
             running = False
         else:
-            name, isv_val = user_input.split()
-            names.append(name)
-            isv.append(isv_val)
+            try:
+                name_unparsed, isv_unparsed = user_input.split()
+                isv_lead , isv_team = isv_unparsed.split("/")
+                print(isv_lead)
+                print(isv_team)
+
+                if not isv_lead.isdigit() or not isv_team.isdigit():
+                    raise ValueError 
+
+            except ValueError:
+                 print("Error: Invalid input, please input correct ISV")
+                 continue
+            names.append(name_unparsed)
+
+
+
         counter -= 1
         if counter == 0:
             running = False
